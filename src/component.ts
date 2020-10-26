cf2bd30e6fb57b96419ea5770302c8907df93874
Chris NeJame <chris.t.nejame@gmail.com>
2020-10-26 10:59:55 -0400

    added JS safe implementation

--- src/component.ts
+++ src/component.ts
@@ -5,7 +5,17 @@ import { Page } from './page';
 export class PageComponent {
   locator: Locator;
   protected _findFromParent: boolean = false;
-  constructor(public parent: PageComponent | Page, public driver: WebDriver) {}
+  protected get _componentMapping(): { [componentName: string]: typeof PageComponent} { return {}};
+  constructor(public parent: PageComponent | Page, public driver: WebDriver, ...args: any[]) {
+    for (let propertyKey in this._componentMapping) {
+      const ComponentClass = this._componentMapping[propertyKey];
+      Object.defineProperty(this, propertyKey, {
+        get: function() {
+          return new ComponentClass(this, this.driver);
+        }
+      });
+    }
+  }
 
   protected async _getReferenceNode(): Promise<WebElement | WebDriver> {
     return this._findFromParent && this.parent instanceof PageComponent ? await this.parent.getElement() : this.driver;
--- src/page.ts
+++ src/page.ts
@@ -1,6 +1,19 @@
 import 'reflect-metadata';
 import { WebDriver } from 'selenium-webdriver';
+import { PageComponent } from './component';
 
 export class Page {
-  constructor(public driver: WebDriver) {}
+  protected get _componentMapping(): { [componentName: string]: typeof PageComponent } {
+    return {};
+  }
+  constructor(public driver: WebDriver, ...args: any[]) {
+    for (let propertyKey in this._componentMapping) {
+      const ComponentClass = this._componentMapping[propertyKey];
+      Object.defineProperty(this, propertyKey, {
+        get: function() {
+          return new ComponentClass(this, this.driver);
+        }
+      });
+    }
+  }
 }
