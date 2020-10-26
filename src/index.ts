d8588aedeb5421e0ea02ec03a840681959874701
Chris NeJame <chris.t.nejame@gmail.com>
2020-10-26 14:48:29 -0400

    remove commented out code

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
 import "reflect-metadata";
 export * from "./page";
 export * from "./component";
-
-
-// class Example {
-//   @Component()
-//   thing: PageComponent;
-//   constructor(public x: string) {
-//     this.x = x;
-//   }
-// }
-
-// const a = new Example('hello');
-// const b = a.thing;
-// console.log(b);
