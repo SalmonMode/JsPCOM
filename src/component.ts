eac07f10791b953e7f67ef255ee7c6c3fce9679e
Chris NeJame <chris.t.nejame@gmail.com>
2020-10-26 13:59:44 -0400

    added method shortcuts

--- src/component.ts
+++ src/component.ts
@@ -25,6 +25,71 @@ export class PageComponent {
     let refNode = await this._getReferenceNode();
     return refNode.findElement(this.locator);
   }
+
+  async clear(): Promise<void> {
+    const element = await this.getElement()
+    return element.clear()
+  }
+  async click(): Promise<void> {
+    const element = await this.getElement()
+    return element.click()
+  }
+  async findElement(locator: Locator): Promise<WebElement> {
+    const element = await this.getElement()
+    return element.findElement(locator);
+  }
+  async findElements(locator: Locator): Promise<WebElement[]> {
+    const element = await this.getElement();
+    return element.findElements(locator);
+  }
+  async getAttribute(attributeName: string): Promise< string | null > {
+    const element = await this.getElement();
+    return element.getAttribute(attributeName);
+  }
+  async getCssValue(cssStyleProperty: string): Promise<string> {
+    const element = await this.getElement();
+    return element.getCssValue(cssStyleProperty);
+  }
+  async getId(): Promise<string> {
+    const element = await this.getElement();
+    return element.getId();
+  }
+  async getRect(): Promise<{height: number, width: number, x: number, y: number}> {
+    const element = await this.getElement();
+    return element.getRect();
+  }
+  async getTagName(): Promise<string> {
+    const element = await this.getElement();
+    return element.getTagName();
+  }
+  async getText(): Promise<string> {
+    const element = await this.getElement();
+    return element.getText();
+  }
+  async isDisplayed(): Promise<boolean> {
+    const element = await this.getElement();
+    return element.isDisplayed();
+  }
+  async isEnabled(): Promise<boolean> {
+    const element = await this.getElement();
+    return element.isEnabled();
+  }
+  async isSelected(): Promise<boolean> {
+    const element = await this.getElement();
+    return element.isSelected();
+  }
+  async sendKeys(...args: any[]): Promise<void> {
+    const element = await this.getElement();
+    return element.sendKeys(...args);
+  }
+  async submit(): Promise<void> {
+    const element = await this.getElement();
+    return element.submit();
+  }
+  async takeScreenshot(scroll?: boolean): Promise<string> {
+    const element = await this.getElement();
+    return element.takeScreenshot(scroll);
+  }
 }
 
 export function Component(...args: any[]) {
